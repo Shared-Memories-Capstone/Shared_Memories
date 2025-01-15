eaa26b7ca7aa12b70b2228ffcb697f1c35c5e517
Kaylee Burch <kaylee.burch@quevera.com>
2025-01-14 21:03:35 -0500

    adding a new custom view to hit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import JsonResponse
+
+
+def test_view(request):
+    data = {"new_count": 2}  # whenever this view is called, it will return this data
+    return JsonResponse(data)
