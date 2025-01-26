6fe3d569ab74c2245b54686068eb2d585c3ed7ce
Michael Hooker <mahooker.16@gmail.com>
2025-01-25 21:53:06 -0500

    Replace sample app with start of Shared Memories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router';
+import AccessCodeCard from './components/AccessCodeCard.jsx';
+import About from './components/About.jsx'
+import CarouselExample from './components/CarouselExample.jsx'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,28 +31,20 @@ function App() {
   // we declare any other functions that we need
   // and then we return the JSX that we want to render
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      {/*Nav bar goes here */}
+      <Header/>
+
+      <Routes>
+        <Route path="/" element={<AccessCodeCard />}/>
+
+        <Route path='/about' element={<About />} />
+
+        <Route path='/CarouselExample' element={<CarouselExample/>} />
+        
+        <Route path='/api-doc' element={<SwaggerUI url="http://127.0.0.1:8000/static/openapi2.json" />} />
+      </Routes>
+    </Router>
   )
 }
 
