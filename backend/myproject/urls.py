ccc7bc0cb26d32b329e24d329df4bff937013217
Michael Hooker <mahooker.16@gmail.com>
2025-01-25 22:17:10 -0500

    Prepare to serve static files with CORS headers
    Will be used to allow cross origin requests from react to django’s static files. A real implementation of this would use a separate server to serve the static OpenAPI document.

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from api.views import test_view
@@ -24,3 +25,6 @@ urlpatterns = [
     path("test/", test_view, name="test"),  # this is the new path we added and is what React is calling to
     path('api/', include('api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
