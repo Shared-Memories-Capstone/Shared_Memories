14f5e887a90772d75c6f820d253c17a735029494
Michael Hooker <mahooker.16@gmail.com>
2025-01-26 22:04:56 -0500

    Replace deprecated react-router-dom package

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header.jsx'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router';
 import AccessCodeCard from './components/AccessCodeCard.jsx';
 import About from './components/About.jsx'
 import CarouselExample from './components/CarouselExample.jsx'
