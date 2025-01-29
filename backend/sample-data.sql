fb2a86b0d840cd624c79b641837dfbe9567dcaaf
Michael Hooker <mahooker.16@gmail.com>
2025-01-28 20:56:56 -0500

    Fix SQL insert field order
    The `name` and `access_code` fields were swapped.

--- backend/sample-data.sql
+++ backend/sample-data.sql
@@ -1,4 +1,4 @@
-INSERT INTO "gallery_event" VALUES (1, 'abcdef', '2023-04-29', 'Brief event description.', 'Maryland Day 2023');
+INSERT INTO "gallery_event" VALUES (1, 'Maryland Day 2023', '2023-04-29', 'Brief event description.', 'abcdef');
 INSERT INTO "gallery_upload" VALUES (1, 'Michael', 'photo1.jpg', '', '2025-01-19 23:31:34.943199', 0, 1);
 INSERT INTO "gallery_upload" VALUES (2, 'Michael', 'photo2.jpg', '', '2025-01-19 23:32:00.155325', 0, 1);
 INSERT INTO "gallery_upload" VALUES (3, 'Michael', 'photo3.jpg', '', '2025-01-19 23:32:58.822702', 0, 1);
