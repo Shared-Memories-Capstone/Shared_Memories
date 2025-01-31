72f182c1e1729685d3cbfb2495064fc444aeeb26
unknown <nagelcj1@gmail.com>
2025-01-30 18:26:10 -0800

    Added event model, view, serializer, urls in backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import ItemListCreateView
+from django.urls import path, include
+from .views import ItemListCreateView, EventViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'events', EventViewSet)
 
 urlpatterns = [
     path('items/', ItemListCreateView.as_view(), name='item-list-create'),
+    path('', include(router.urls)),  # Includes all API routes
 ]
