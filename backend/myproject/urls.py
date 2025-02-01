50eccde40283c5ecd78841d75fac31964d74940e
Kaylee Burch <kaylee.burch@quevera.com>
2025-01-31 21:02:57 -0500

    connected openapi to backend mvc

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -14,16 +14,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from api.views import test_view
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
-    path("admin/", admin.site.urls), 
-    path("test/", test_view, name="test"),  # this is the new path we added and is what React is calling to
-    path('api/', include('api.urls')),
+    path("admin/", admin.site.urls),
+    path(
+        "test/", test_view, name="test"
+    ),  # this is the new path we added and is what React is calling to
+    path("api/", include("api.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
 
 if settings.DEBUG:
