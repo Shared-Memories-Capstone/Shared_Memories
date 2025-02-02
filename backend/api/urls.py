ef747b78a655538ef0a32e09af4fa125c6e7ebb9
Kaylee Burch <kaylee.burch@quevera.com>
2025-02-02 10:58:29 -0500

    add ability to upload photo. Image saves to media folder in backend, and photo data saves to Photo model.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
-from .views import EventViewSet, PhotoViewSet, login, register, verify_token
+from .views import (
+    EventViewSet,
+    PhotoViewSet,
+    login,
+    register,
+    verify_token,
+    upload_photo,
+)
 from rest_framework.routers import DefaultRouter
 
 
@@ -12,4 +19,5 @@ urlpatterns = [
     path("auth/login/", login, name="login"),
     path("auth/register/", register, name="register"),
     path("auth/verify/", verify_token, name="verify-token"),
+    path("upload-photo/", upload_photo, name="upload_photo"),
 ]
