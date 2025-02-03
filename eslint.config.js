2981aebba9138bda13ab0497eff17cdf2adcdaab
Michael Hooker <mahooker.16@gmail.com>
2025-02-02 22:17:58 -0500

    Add sample eslint config

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// eslint.config.js
+module.exports = [
+  {
+    rules: {
+      semi: "error",  // use semicolons at the end of statements
+      "prefer-const": "error", // use const when variables aren't reassigned
+      "eqeqeq": "error",  // require === instead of ==
+      "no-unused-vars": "warn",  // warn on declared but unused variables
+      "no-console": "warn",  // warn if console.log is present
+      "indent": ["warn", 2],  // enforce 2-space indentation (but just warn)
+      "curly": "warn",  // encourage braces for if/else
+      "no-multiple-empty-lines": ["warn", { "max": 1 }]  // limit empty lines to 1
+    }
+  }
+];
