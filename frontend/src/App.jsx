0a932966cbb5e6d60242ae58b1b86cd46be2d7e6
Michael Hooker <mahooker.16@gmail.com>
2025-02-17 13:04:07 -0500

    Fix broken API URL reference
    The API link was working correctly in the production Docker environment but not in the development environment. Read from the VITE_API_URL environment variable or default to the development backend URL and port.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,8 @@ import ProtectedRoute from './components/ProtectedRoute.jsx';
 import CreateEventForm from './components/CreateEventForm';
 import EventPage from './components/EventPage.jsx';
 import EventManager from './components/EventManager.jsx';
+// Use Vite's env variable (make sure it’s prefixed with VITE_)
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 
 function App() {
 
@@ -39,7 +41,7 @@ function App() {
        {/* Protected routes */}
         <Route path='/api-doc' element={
           <ProtectedRoute>
-            <SwaggerUI url="/api/schema/" />
+            <SwaggerUI url={`${API_URL}/schema/`} />
           </ProtectedRoute>
         } />
         <Route
