55750a97f5d9954c9ed58e3cc0eb47ba491350e9
Michael Hooker <mahooker.16@gmail.com>
2025-02-27 20:24:29 -0500

    Update entrypoint to disable gunicorn logging
    This reverts commit 70662ae2534ff9771a4b570d9cecd23a374457f7 and disables gunicorn logging. I’ll revisit this decision if I have time later.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,11 +5,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
-exec gunicorn --bind 0.0.0.0:8000 \
-    --access-logfile /var/log/gunicorn-access.log \
-    --error-logfile /var/log/gunicorn-error.log \
-    --log-level debug \
-    --capture-output \
-    --enable-stdio-inheritance \
-    myproject.wsgi:application \
-	"$@"
+exec gunicorn --bind 0.0.0.0:8000 myproject.wsgi:application "$@"
